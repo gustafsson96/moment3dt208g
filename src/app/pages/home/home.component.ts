5ada602f1a3098199076afb1d7afc9d288ed7854
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-23 20:51:10 +0200

    Import NgOptimizedImage and update img tags accordingly

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../button/button.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
