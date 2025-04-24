4818fef14f4372dcba06a7ce510f07197e89c918
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-24 10:09:16 +0200

    Delete button component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from '../../button/button.component';
 import { NgOptimizedImage } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent, NgOptimizedImage, RouterLink],
+  imports: [NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
